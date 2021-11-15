4777d1eb7c027f50708a890df8997b1c931088bc
DESKTOP-DRHRRQM\cristian <cristianpg123@hotmail.com>
2021-11-15 18:31:13 -0500

    cambio 11/15/2021

--- src/main/java/co/com/mintic/tiendagenerica/SpringBootSecurityJwtApplication.java
+++ src/main/java/co/com/mintic/tiendagenerica/SpringBootSecurityJwtApplication.java
@@ -2,12 +2,20 @@ package co.com.mintic.tiendagenerica;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootSecurityJwtApplication {
+public class SpringBootSecurityJwtApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootSecurityJwtApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(SpringBootSecurityJwtApplication.class);
 	}
 
 }
